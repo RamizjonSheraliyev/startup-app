1866690f50140ac80f37ee79c76dabc6261699bb
ramizjo <ramizsheraliyev8@gmail.com>
2025-02-16 16:36:19 +0500

    code uptades webhook

--- hooks/use-user.ts
+++ hooks/use-user.ts
@@ -5,28 +5,32 @@ import { useEffect, useState } from 'react'
 import { useRefresh } from './use-refresh'
 
 const useUser = () => {
-	const [user, setUser] = useState<IUser | null>(null)
+  const [user, setUser] = useState<IUser | null>(null)
 
-	const { onOpen } = useRefresh()
-	const { userId } = useAuth()
+  const { onOpen } = useRefresh()
+  const { userId } = useAuth()
 
-	useEffect(() => {
-		const getData = async () => {
-			try {
-				const data = await getUser(userId!)
-				data === 'notFound' && onOpen()
-				setUser(data)
-			} catch (error) {
-				setUser(null)
-			}
-		}
+  useEffect(() => {
+    if (!userId) return // Null yoki undefined bo‘lsa, hech narsa qilmaydi
 
-		userId && getData()
+    const getData = async () => {
+      try {
+        const data = await getUser(userId)
+        if (data === 'notFound') {
+          onOpen() // Not found bo‘lsa, refresh modal ochadi
+        } else {
+          setUser(data)
+        }
+      } catch (error) {
+        console.error('Failed to fetch user:', error)
+        setUser(null) // Xatolik bo‘lsa, foydalanuvchini null qilamiz
+      }
+    }
 
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [])
+    getData()
+  }, [userId, onOpen]) // Dependensiyalarni to‘g‘ri qo‘shib qo‘yamiz
 
-	return { user }
+  return { user }
 }
 
 export default useUser
